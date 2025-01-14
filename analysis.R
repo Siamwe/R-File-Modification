ab06c0f74a2213aad642317f551fcbb1632ff583
Siamwe <siaisaria.mwende@wmich.edu>
2025-01-14 13:26:20 -0500

    Initial commit:Added analysis.R

--- analysis.R
+++ analysis.R
@@ -12,3 +12,12 @@ print(message)
 print(paste("Hello", "World"))
 
 
+#Function 
+square_root <- function(x) {
+return (x^0.5)
+}
+
+
+#Testing the function
+result <- square_root(25)
+print(paste("The square root of 25 is:", result))
